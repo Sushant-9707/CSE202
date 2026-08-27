9ffd3d8ea8a806183bbdbe6e193b2e0239d851b3
Sushant Kumar <sushantkumar9707@gmail.com>
2026-08-27 14:18:34 +0530

    git commit -m "string2"

--- class2.cpp
+++ class2.cpp
@@ -1,28 +1,42 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 class mystring{
     public:
 
-    string s;
+    char s[30];
+    int n = sizeof(s)/sizeof(s[0]);
     
     mystring(){
-        cout << "Enter the string";
+        cout << "Enter the string: ";
         cin >> s;
     }
 
     void displaystr(){
         cout << s << endl;
     }
+
+    void find(char a){
+        for(int i = 0; i < n; i++){
+            if(s[i] == a){
+                cout << "Found character at index " << i << endl;
+            }
+        }
+    }
 };
 
 
 int main(){
 
-    mystring os;
+    mystring os1;
 
-    os.displaystr();
+    os1.displaystr();
+    os1.find('s');
+
+    mystring os2;
+
+
+    return 0;
 
 
 }
